1dcd4c6d7a6148e55d32d150a8b811da6c36781a
Andrej Hristov <andrejhris23@gmail.com>
2022-04-29 12:27:15 +0200

    add: axios

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
 import type { NextPage } from 'next';
+import { useEffect } from 'react';
 import Example from '../components/example';
 import Inside from '../components/inside';
+import Axios from '../utils/axios';
 
 const Home: NextPage = () => {
+  useEffect(() => {
+      Axios.post('http://localhost:8080/user/create', {
+        user: {
+          email: 'andr@gmail.com',
+          displayName: 'and gr',
+          firstName: 'an',
+          lastName: 'gr',
+          image: 'https://twitter.com'
+        }
+      })
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err))
+  }, []);
+
   return <Example />;
   // return <Inside />
 };
