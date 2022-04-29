ee0aea313804bd69e3d1b298a0607e45f2d30201
Andrej Hristov <andrejhris23@gmail.com>
2022-04-29 14:36:24 +0200

    fix: wrap a tag in link comp

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -152,13 +152,13 @@ export default function Navigation() {
                     </Menu.Items>
                   </Transition>
                 </Menu>
-
+                <Link href='/createEvent'>
                 <a
-                  href='/createEvent'
                   className='ml-6 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-rose-600 hover:bg-rose-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500'
                 >
                   New Event
                 </a>
+                </Link>
               </div>
             </div>
           </div>
