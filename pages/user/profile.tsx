f0d495ab943bfd92867475de836d7dbd16b877ae
Andrej Hristov <andrejhris23@gmail.com>
2022-04-30 00:07:07 +0200

    add: example server side fetching

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -1,17 +1,25 @@
-import type { NextPage } from 'next';
+import type { NextPage, GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { Fragment } from 'react';
-
+import Axios from '../../utils/axios';
 import Setting from '../../components/setting';
 import ProfileNavigation from '../../components/profileNavigation';
 import Profile from '../../components/profile';
 
-const profile: NextPage = () => {
+const profile: NextPage = ({user} : InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  console.log(user);
+
   return (
     <Fragment>
-      
       <Profile />
     </Fragment>
   );
 };
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await Axios.get('http://localhost:8080/user/findById/2');
+  const user = res.data;
+
+  return { props: { user }}
+}
 
 export default profile;
