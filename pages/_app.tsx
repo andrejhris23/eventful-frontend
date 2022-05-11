7f9e1c81b35485a8157798ccc9efffbc84624dc8
Andrej Hristov <andrejhris23@gmail.com>
2022-05-11 15:17:31 +0200

    update: no layout on landing page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,20 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import Layout from '../components/layout';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const router = useRouter();
+
   return (
       <SessionProvider session={pageProps.session}>
+        {router.pathname === '/' ? 
+        <Component {...pageProps} /> :
         <Layout>
           <Component {...pageProps} />
         </Layout>
+        }
       </SessionProvider>
   );
 }
